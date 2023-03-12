29984017ccea696b9fee9d94e90260551389472d
Anuj Yadav <127096845+anujgitaccount@users.noreply.github.com>
2023-03-12 15:34:23 +0530

    Delete GetInstanceMetaDataValue.ps1

--- GetInstanceMetaDataValue.ps1
+++
@@ -1,7 +0,0 @@
-$result = $metadata = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri "http://169.254.169.254/metadata/instance?api-version=2020-06-01"
-$data = Read-Host -Prompt "Enter the data you wish to search - compute / network "
-$key = Read-Host -Prompt "Enter the key you wish to search"
-
-$value = $result.$data.$key
-
-Write-Host "$key is $value"
